2e8621be98c2b0b59db51fff677b96f731c5dba4
Magali <magalilaparamirez@gmail.com>
2022-10-18 15:49:10 -0500

    mi tarea de los dos aberiguaciones del dia martes

---
+++ 1.tiposDeDato/ejercicos.js
@@ -0,0 +1,7 @@
+//averiguar el metodo de javascrinpt que mi permite saber que tipo de dato tengo
+// tarea
+//para comprobar el tipo de un dato es typeof
+//typeof operando
+//]typeof (operando)
+//el operador typeof devuelve una cadena que indica el tipo
+//operando es la cadena variable, palabra clave
--- 2.variables/ejercicios.js
+++ 2.variables/ejercicios.js
@@ -1,1 +1,17 @@
 // averiguar las deferincias entre let y var
+// la cocina del codigo
+//hosting, scope
+//contxto global y local
+//let es el que permite declarar variable
+//limitado su alcance al bloque, declaracion o expresion donde esta usando la clave
+
+//var es la cual que define variable global o local de una funcion sin importante 
+
+function vasTest() {
+    var x = 31;
+    if (true) {
+        var x =71; // ¡misma variable!
+console.log(x); // 71
+    }
+    console.log(x);  //71
+}
