d3acb4733ee545467db3ad099ae1d53daf452ea5
Magali <magalilaparamirez@gmail.com>
2022-11-03 10:46:12 -0500

    clase ejercicios de for del dia de hoy 03 noviembre

--- 5.estructuraDecision/index.js
+++ 5.estructuraDecision/index.js
@@ -1,14 +1,34 @@
 // la estructura de decisionseguira un camino destintos segun la decision
-numeroMes=enero
+numeroMes=5
 switch(numeroMes){
     case 1:
         console.log("enero")
-        case 2:
-            console.log("febrero")
-            case 3:
-                console.log("marzo") 
-                break
-                default:
-                    console.log("error mes no encontrado") error mes no encontrado
+    case 2:
+     console.log("febrero")
+    case 3:
+     console.log("marzo") 
+break
+ default:
+ console.log("error mes no encontrado") error mes no encontrado
 
 }
+
+numeroORDEN=6
+switch(numeroORDEN){
+    case 1:
+        console.log("uno")
+    case 2:
+            console.log("dos")
+break
+default:
+console.log("error")
+
+vocal="a"
+switch(vocal){
+    case "a": case "e": case "i": case "o": case "u":
+        console.log("la letra"+ "vocal"+ "es una vocal") la letra e es una vocal
+    break
+ default:
+    console.log("no es una vocal")
+ }
+
--- 6.estructuraCiclo/ejercicio.js
+++ 6.estructuraCiclo/ejercicio.js
@@ -1,28 +1,67 @@
 //averiguar su concepto aplicacion y ejercicios
 //existen 3
 //for
-for(let i=0;i<=10;i+=2) {
-    console.log(i+2);
+for(let inicial=1;inicial<=20;inicial++) {
+    console.log(inicial)
+    
+    }
+
+
+
+    for(let inicial =1;inicial<=20;inicial++){
+      if(inicial%2==0){
+        console.log(inicial)
+      }else{
+        console.log(inicial)
+      }
+    }
+
+
+    let vocales="aeiou"
+  console.log(vocales.length)
+  for(let i=0;i<vocales.length;i++){
+    console.log(vocales[i])
+  }
+  
+//let texto="hola como estas como te llamas"
+//let contador=0
+//for(let i=0;i<texto.length;i++){
+ // if(texto[i]=="a"){
+   // contador+=1
+  //}
+//}
+//console.log("tienes",contador,"vocales a")
+//for
+let texto="eucalipto"
+let contador=0
+for(let i=0;i<texto.length;i++){
+ 
+  if(texto[i]=="a"){
+    contador+=1
+
+    if(texto[i]=="e"){
+      contador+=1
+
+      if(texto[i]=="u"){
+        contador+=1
+
+        if(texto[i]=="o"){
+          contador+=1
+
+          if(texto[i]=="i"){
+            contador+=1
+
+ }
 
 }
-//while
-let eval =true
-let eval2 =""
-while(eval==true) {
-if (eval2=="salir"){
-    console.log("adios") adios
-    eval= false
-}
-console.log("hola")  hola
-}
-//do while
-let result = '';
-let i = 0;
+console.log("tienes",contador,"vocales 5")
 
-do {
-  i = i + 1;
-  result = result + i;
-} while (i < 5);
 
-console.log(result);
-// expected result: "12345"
+
+//hacer un programa que se muestrael factorialde un numero 
+5!=5*4*3*2*1
+5!=120
+//que mi determine si un texto es palimdromo
+ada
+//q mi muestre los n numeros de la siere de fibonacci
+1-2-3-5-8-13-21-34
