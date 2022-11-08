2fc1f58b244397e02a2077f7f3b00c1ff29d7798
Magali <magalilaparamirez@gmail.com>
2022-11-08 11:15:41 -0500

    ejercicio 1

--- 7.funciones/ejercicio.js
+++ 7.funciones/ejercicio.js
@@ -2,11 +2,25 @@
 //contarVocales("eucalipto") //5
 
 let texto="eucalipto"
+let contador=0
 for (let i=0;i<texto.length;i++) {
-    switch(texto[i]){
-        case "a"
+    if(texto[i]=="e"){
+        contador+=1
     }
+    if(texto[i]=="u"){
+        contador+=1
+    }
+    if(texto[i]=="a"){
+        contador+=1
+    }
+    if(texto[i]=="o"){
+        contador+=1
+    } if(texto[i]=="u"){
+        contador+=1
+    }
+    return contador;
 }
+console.log("tiene,contador)
 
 
 //funcion calculadora(num1,num2,operacion){retur total}
---
+++ 7.funciones/index.js
@@ -0,0 +1,19 @@
+//arrow funcion ofunciones flecha
+//pasos para construiruna funcion 
+//1. palabra reservada funciones
+//2. ponerle un nombre que describa el trabajo
+//que realiza la funcion 
+//3. la funcion puede resivir parametros
+//pero si o si indicar que tendra un parametros
+//()
+//(listaAmigos)
+//(edad, nombre)
+//4. DATO IMPORTANTE: Una funcion siempre tiene que retomar un tipo de dato
+
+function saludo(nombre,apellido){
+    let saludo="hola como estas "+nombre+" "+ apellido
+    return saludo
+}
+// para usar una funcion debo llamar a la funcion  
+console.log(saludo("magaly","Lapa","Ramirez"))
+console.log(saludo("luz","magaly"))
