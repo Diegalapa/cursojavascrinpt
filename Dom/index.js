ec2ab9569f84510a665f79bf310bc78d39fa1f28
Magali <magalilaparamirez@gmail.com>
2022-11-17 11:08:53 -0500

    dom

---
+++ Dom/index.js
@@ -0,0 +1,10 @@
+//DOM: significa Document Object Model, en español sería Modelo de Objetos del Documento. Es una interfaz de programación que nos permite crear, cambiar, o remover elementos del documento. También podemos agregar eventos a esos elementos para hacer más dinámica nuestra página.
+
+//El DOM visualiza el documento de HTML como un árbol de tres nodos. Un nodo representa un documento de HTML.
+
+//Echemos un vistazo a este código de HTML para entender mejor la estructura de árbol del DOM.
+
+//Nuestro documento se llama nodo raíz y contiene un nodo hijo el cual es el elemento <html>. El elemento <html> contiene dos hijos los cuales son los elementos <head> y <body>.
+
+//Ambos elementos <head> y <body> tienen hijos propios.
+
